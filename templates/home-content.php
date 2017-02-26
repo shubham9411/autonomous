4d5607d5f44a405b61fbfe256a551287651dd6fd
shubham9411 <shubham9411@gmail.com>
2017-02-26 15:15:27 +0530

    Navbar and card links headings underline

--- templates/home-content.php
+++ templates/home-content.php
@@ -18,20 +18,20 @@
 		</div>
 	</div>
 	<div class="col-xs-12 col-sm-6 ">
-		<div class="card event" id="event">
-			<?php $category_id = get_cat_ID( 'events' ); ?>
-			<h4><a href="<?php echo esc_url( get_category_link( $category_id ) );?>" title="Events">Events</a></h4>
-			<ul class="no-list">
-				<?php anomous_cards_home( 'events' ); ?>
-			</ul>
-		</div>
-	</div>
-	<div class="col-xs-12 col-sm-6 ">
 		<div class="card news" id="news">
 			<?php $category_id = get_cat_ID( 'news' ); ?>
 			<h4><a href="<?php echo esc_url( get_category_link( $category_id ) );?>" title="News">News</a></h4>
 			<ul class="no-list">
 				<?php anomous_cards_home( 'news' ); ?>
+			</ul>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-6 ">
+		<div class="card event" id="event">
+			<?php $category_id = get_cat_ID( 'events' ); ?>
+			<h4><a href="<?php echo esc_url( get_category_link( $category_id ) );?>" title="Events">Events</a></h4>
+			<ul class="no-list">
+				<?php anomous_cards_home( 'events' ); ?>
 			</ul>
 		</div>
 	</div>
