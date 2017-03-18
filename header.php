edf93d47b1e84294999cf9804a424484fb13e045
rohitjuyal5 <rohitjuyal5@gmail.com>
2017-03-18 22:47:37 +0530

    Done

--- header.php
+++ header.php
@@ -14,15 +14,48 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<?php wp_head(); ?>
+		<body>
+<div id="text-carousel" class="carousel slide" data-ride="carousel">
+    <!-- Wrapper for slides -->
+    <div class="row">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="carousel-content">
+                        <div>
+                            <p>Student are informed to report before specified date.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-content">
+                        <div>
+                            <p>Worshop will take place btwn these dates.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-content">
+                        <div>
+                            <p>us provident deleniti tenetur iusto officiis recusandae corporis culpa quaerat?</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+
+</div>
+</body>
+
 	</head>
 	<body <?php body_class(); ?> data-spy="scroll" data-target=".navbar" data-offset="50">
 		<div class="page">
 			<header class="header" id="header">
-			
+
 				<?php get_template_part( 'templates/header/header' , 'masthead' ); ?>
 
 				<?php get_template_part( 'templates/header/header' , 'navigation' ); ?>
-			
+
 			</header> <!-- .header-->
 
 			<div class="content">
